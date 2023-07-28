74a2acbcd5250e7a632843c281f14ef8426eb4a6
VitaliiGalayko <107875708+vitalii0932@users.noreply.github.com>
2023-07-29 00:36:39 +0300

    Create Human.java

---
+++ Lesson9HW/src/Exercise4/Human.java
@@ -0,0 +1,27 @@
+package Exercise4;
+
+public class Human {
+    private String name;
+
+    public Human() {
+    }
+
+    public Human(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
