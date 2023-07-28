83fe9b312624c41a86c62ec7858e30ea59423f2d
VitaliiGalayko <107875708+vitalii0932@users.noreply.github.com>
2023-07-29 00:37:12 +0300

    Create StudentLastNameComparator.java

---
+++ Lesson9HW/src/Students/src/BasicLevel/StudentLastNameComparator.java
@@ -0,0 +1,22 @@
+package Students.src.BasicLevel;
+
+import java.util.Comparator;
+
+public class StudentLastNameComparator implements Comparator {
+    @Override
+    public int compare(Object o1, Object o2) {
+        Student student1 = (Student) o1;
+        Student student2 = (Student) o2;
+        String lastName1 = student1.getLastName();
+        String lastName2 = student2.getLastName();
+
+        if(lastName1.compareTo(lastName2) > 0) {
+            return 1;
+        }
+        if(lastName1.compareTo(lastName2) < 0) {
+            return -1;
+        }
+
+        return 0;
+    }
+}
